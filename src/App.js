a453387219da12d19acd9aa8ced431ae7a726169
Morteza erfani <m.erfani74@gmail.com>
2022-07-25 11:51:57 +0430

    cart context provider complete

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import ProductContextProvider from "./context/ProductContextProvider";
 // Component
 import Store from "./component/Store";
 import ProductDetails from "./component/ProductDetails";
+import CartContextProvider from "./context/CartContextProvider";
 
 function App() {
   return (
     <div className="App">
       <ProductContextProvider>
-        <Routes>
-          <Route path="/products/:id" element={<ProductDetails/>} />
-          <Route path="/products" element={<Store />} />
-          <Route path="/*" element={<Navigate to="/products" />} />
-        </Routes>
+        <CartContextProvider>
+          <Routes>
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/products" element={<Store />} />
+            <Route path="/*" element={<Navigate to="/products" />} />
+          </Routes>
+        </CartContextProvider>
       </ProductContextProvider>
     </div>
   );
