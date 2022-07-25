796e1b1d21e5b047c722ed68ba72fac9950185fc
Morteza erfani <m.erfani74@gmail.com>
2022-07-25 16:47:55 +0430

    product buttons functionality complete

--- src/component/shared/Product.js
+++ src/component/shared/Product.js
@@ -1,10 +1,18 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 // Functions
-import { shorter } from "../../helpers/functions";
+import { shorter, quantityCount } from "../../helpers/functions";
+
+// Context
+import { CartContext } from "../../context/CartContextProvider";
+
+// Icons
+import trashIcon from "../../assets/icons/icons8-trash.svg";
 
 const Product = ({ productData }) => {
+  const {state, dispatch} = useContext(CartContext);
+
   return (
     <div>
       <img src={productData.image} alt="product" style={{ width: "200px" }} />
@@ -13,7 +21,47 @@ const Product = ({ productData }) => {
       <div>
         <Link to={`/products/${productData.id}`}>Detail</Link>
         <div>
-          <button>Add To Cart</button>
+          {quantityCount(state, productData.id) === 1 && (
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: productData })
+              }
+            >
+              <img src={trashIcon} alt="trash" />
+            </button>
+          )}
+
+          {quantityCount(state, productData.id) > 1 && (
+            <button
+              onClick={() =>
+                dispatch({ type: "DECREASE", payload: productData })
+              }
+            >
+              -
+            </button>
+          )}
+
+          {quantityCount(state, productData.id) > 0 && (
+            <span>{quantityCount(state, productData.id)}</span>
+          )}
+
+          {quantityCount(state, productData.id) > 0 ? (
+            <button
+              onClick={() =>
+                dispatch({ type: "INCREASE", payload: productData })
+              }
+            >
+              +
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                dispatch({ type: "ADD_ITEM", payload: productData })
+              }
+            >
+              Add To Cart
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -3,3 +3,12 @@ export const shorter = (title) => {
   const newTitle = `${splitedTitle[0]} ${splitedTitle[1]}`;
   return newTitle;
 };
+
+export const quantityCount = (state, id) => {
+  const index = state.selectedItems.findIndex((items) => items.id === id);
+  if (index === -1) {
+    return false;
+  } else {
+    return state.selectedItems[index].quantity;
+  }
+};
